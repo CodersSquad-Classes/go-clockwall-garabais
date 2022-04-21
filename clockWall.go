9429bf53e48237b53dd59961b879bcc3577ba541
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2022-04-21 17:34:22 -0500

    Add ClockWall implementation for multiple clients

--- clockWall.go
+++ clockWall.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func dialClock(zone, address string, printer chan string) {
+
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Printf("Unable to connect to %v, skipping\n", address)
+		return
+	}
+
+	buf := make([]byte, 256)
+	for {
+		n, err := conn.Read(buf)
+
+		if n == 0 || err != nil {
+			return
+		}
+
+		printer <- fmt.Sprintf("%v\t : %s", zone, buf)
+	}
+}
+
 func main() {
-	// Let's start the fun
+	times := make(chan string, len(os.Args)-1)
+	defer close(times)
+
+	for _, arg := range os.Args[1:] {
+		v := strings.Split(arg, "=")
+		if len(v) != 2 {
+			fmt.Printf("Invalid argument `%v`, skipping\n", arg)
+		}
+		go dialClock(v[0], v[1], times)
+	}
+
+	for time := range times {
+		fmt.Print(time)
+	}
 }

3b8f54e58e5192c04b1e2dd84b3696c3ca194d03
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2022-04-21 16:44:48 -0500

    Add port and timezone to the server
    The server program now needs the port to be passed as an argument using
    the `p` flag. If not port is passed, the program won't run.
    It also set the timezone in which the server with run using the
    environment variable `TZ`, if the variable is empty or is an invalid
    timezone the program use UTC instead.

--- clockServer.go
+++ clockServer.go
@@ -2,16 +2,19 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
-func handleConn(c net.Conn) {
+func handleConn(c net.Conn, loc *time.Location) {
 	defer c.Close()
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, time.Now().In(loc).Format("15:04:05\n"))
 		if err != nil {
 			return // e.g., client disconnected
 		}
@@ -20,16 +23,27 @@ func handleConn(c net.Conn) {
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+	p := flag.Int("p", 0, "Port in whic the server will run")
+	flag.Parse()
+	if *p <= 0 {
+		log.Fatal("Invalid port")
+	}
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *p))
 	if err != nil {
 		log.Fatal(err)
 	}
+	loc, err := time.LoadLocation(os.Getenv("TZ"))
+	if err != nil {
+		println("Invalid time zone '%s' using UTC instead.")
+		loc = time.UTC
+	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		go handleConn(conn) // handle connections concurrently
+		go handleConn(conn, loc) // handle connections concurrently
 	}
 }
